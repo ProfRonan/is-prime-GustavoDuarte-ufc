94d740ea12b88dcee3b9ed2d92a980555a0a3f52
GustavoDuarte-ufc <gustavoduarte2804@gmail.com>
2023-04-10 17:53:12 -0300

    Modificado

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 a = int(input())
 b = 0
-for i in range(1, a+1):
-    if a % i == 0:
+c = 1
+while c < a+1:
+    if a % c == 0:
         print(end='')
         b += 1
     else:
         print(end='')
+    c = c + 1
 if b == 2:
     print("Primo")
 elif b != 2 and a > 0:
     print("Não primo")
 elif a <= 0:
     print("Número inválido")
+
 
 
     
